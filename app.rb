9b455a2d29061781d9569e5f41728080af221ba9
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-06-14 16:14:13 +0100

    params

--- app.rb
+++ app.rb
@@ -9,7 +9,13 @@ get '/secret' do
     'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
     @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
+end
+
+get '/named-cat' do
+    p params
+    @name = params[:name]
     erb(:index)
 end
